f6cd86cb794db5a56bc7a43883d4c5eb1e9fe89c
OwOimmc <vacata@frogi.cz>
2023-02-04 04:17:47 +0100

    feat: víc variabilní

--- src/views/index/lol.vue
+++ src/views/index/lol.vue
@@ -1,10 +1,18 @@
 <template>
-  <div class="p flex items-center justify-center">
+  <div
+    :class="{ 'bg-[#ffeb3b]': isYellow, 'bg-[#0095ff]': !isYellow }"
+    class="p flex items-center justify-center"
+  >
     <div class="main max-w-[120rem] my-50 w-full grid justify-between">
       <div
         v-for="(b, i) in arrBlocks"
         :key="`b-${i}`"
-        :style="{ 'grid-area': b.x + '/' + b.y }"
+        :style="{
+          'grid-area': b.x + '/' + b.y,
+          width: `${b.box}rem`,
+          height: `${b.box}rem`,
+          background: b.acutal
+        }"
         class="block"
       ></div>
     </div>
@@ -14,40 +22,79 @@
 <script setup>
 import { ref } from 'vue'
 
-const countDown = ref(true)
+const isTimerMoveBox = ref(true)
+const isTimerStyleBox = ref(true)
+const isTimerStyleWrapper = ref(true)
 
-const countDownTimer = () => {
-  if (countDown.value) {
+const isYellow = ref(true)
+
+const timerMoveBox = () => {
+  if (isTimerMoveBox.value) {
     setTimeout(() => {
-      doIt()
-      // countDown.value -= 1
-      countDownTimer()
-    }, 500)
+      move()
+      timerMoveBox()
+    }, 1000)
+  }
+}
+const timerStyleBox = () => {
+  if (isTimerStyleBox.value) {
+    setTimeout(() => {
+      style()
+      timerStyleBox()
+    }, 1000)
+  }
+}
+
+const timerStyleWrapper = () => {
+  if (isTimerStyleWrapper.value) {
+    setTimeout(() => {
+      style()
+
+      isYellow.value = !isYellow.value
+
+      timerStyleWrapper()
+    }, 250)
   }
 }
 
 const arrBlocks = ref([
-  { x: 1, y: 2 },
-  { x: 2, y: 2 },
-  { x: 1, y: 3 },
-  { x: 4, y: 1 },
-  { x: 2, y: 4 },
-  { x: 3, y: 3 }
+  { x: 1, y: 2, box: 3, bg1: '#0095ff', bg2: '#ffeb3b', acutal: '#0095ff' },
+  { x: 2, y: 2, box: 3, bg1: '#0095ff', bg2: '#ffeb3b', acutal: '#0095ff' },
+  { x: 1, y: 3, box: 3, bg1: '#0095ff', bg2: '#ffeb3b', acutal: '#0095ff' },
+  { x: 4, y: 1, box: 3, bg1: '#0095ff', bg2: '#ffeb3b', acutal: '#0095ff' },
+  { x: 2, y: 4, box: 3, bg1: '#0095ff', bg2: '#ffeb3b', acutal: '#0095ff' },
+  { x: 3, y: 3, box: 3, bg1: '#0095ff', bg2: '#ffeb3b', acutal: '#0095ff' }
 ])
 
-const go = (axis) => {
-  axis <= 4 ? axis++ : (axis = 1)
-  return axis
+// const go = (axis) => {
+//   axis <= 4 ? axis++ : (axis = 1)
+//   return axis
+// }
+
+const getRadnom = (max) => {
+  return Math.floor(Math.random() * max)
 }
 
-const doIt = () => {
-  arrBlocks.value.forEach((b) => {
-    b.x = go(b.x)
-    b.y = go(b.y)
-  })
+const move = () => {
+  for (let index = 0; index < arrBlocks.value.length; index++) {
+    arrBlocks.value[index].x = getRadnom(6)
+    arrBlocks.value[index].y = getRadnom(6)
+  }
 }
 
-countDownTimer()
+const style = () => {
+  for (let index = 0; index < arrBlocks.value.length; index++) {
+    arrBlocks.value[index].box = getRadnom(10)
+    arrBlocks.value[index].acutal =
+      getRadnom(2) === 1
+        ? arrBlocks.value[index].bg2
+        : arrBlocks.value[index].bg1
+  }
+}
+
+timerMoveBox()
+timerStyleBox()
+timerStyleWrapper()
 </script>
 
 <style lang="less" scoped>
@@ -55,8 +102,7 @@ countDownTimer()
   --w: 12rem;
   height: 100vh;
   width: 100%;
-  animation: blueToYellow 1s linear infinite;
-  // animation: blueToYellow 800s linear infinite;
+  // animation: blueToYellow 2s linear infinite;
 }
 
 .main {
@@ -68,100 +114,35 @@ countDownTimer()
 }
 
 .block {
-  width: 10rem;
-  height: 10rem;
+  width: 3rem;
+  height: 3rem;
   // animation: yellowToBlue 500s linear infinite;
-  transition: all 3s;
-
-  animation: yellowToBlue 0.4s linear infinite;
-
-  // &:nth-child(1) {
-  //   grid-area: 1 / 1;
-  // }
-  // &:nth-child(2) {
-  //   grid-area: 1 / 5;
-  // }
-  // &:nth-child(3) {
-  //   grid-area: 2 / 3;
-  // }
-  // &:nth-child(4) {
-  //   grid-area: 1 / 2;
-  // }
-  // &:nth-child(5) {
-  //   grid-area: 4 / 1;
-  // }
-  // &:nth-child(6) {
-  //   grid-area: 5 / 4;
-  // }
+  transition: width 2s, height 2s;
 }
 
-// .main {
-//   font-size: 10rem;
-//   position: relative;
-//   color: #fff;
-//   > div {
-//     display: flex;
-//     flex-direction: column;
-//     justify-items: center;
-//     align-items: center;
-//     line-height: 8rem;
-//     margin: 0 1rem;
-//     // filter: blur(0.5rem);
-//     &:hover {
-//       animation: focus 0.4s;
-//       filter: blur(0rem);
-//       span {
-//         animation: fadeIn 0.4s;
-//         opacity: 1;
-//       }
-//     }
-
-//     span {
-//       animation: fadeOut 0.4s;
-//       opacity: 0;
-//       position: relative;
-//       border-top: var(--w) solid #fff;
-//       border-right: var(--w) solid #fff;
-//       border-left: var(--w) solid #fff;
-//       display: block;
-//       height: 1.5rem;
-//       width: 130%;
-//       margin: 0.5rem 0 0 5%;
-//       &:last-child {
-//         transform: rotateX(180deg);
-//       }
-//       &:before {
-//         position: absolute;
-//         content: '';
-//         width: 50%;
-//         height: var(--w);
-//         background-color: var(--color);
-//         z-index: 2;
-//         top: -5px;
-//         left: 50%;
-//         transform: translateX(-50%);
-//       }
-//     }
-//   }
-// }
-
 @keyframes blueToYellow {
-  from {
-    // filter: blur(0.4rem);
+  0% {
     background-color: #0095ff;
   }
-  to {
+  100% {
     background-color: #ffeb3b;
-    // filter: blur(0rem);
   }
 }
 
 @keyframes yellowToBlue {
-  from {
+  25% {
+    background-color: #ffeb3b;
+    // filter: blur(0.4rem);
+  }
+  50% {
+    background-color: #0095ff;
+    // filter: blur(0rem);
+  }
+  75% {
     // filter: blur(0.4rem);
     background-color: #ffeb3b;
   }
-  to {
+  100% {
     background-color: #0095ff;
     // filter: blur(0rem);
   }
